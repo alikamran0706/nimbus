61a946f2552cee3d868a35064c886d29328d24ef
alikamran0706 <alikamrantechdev@gmail.com>
2025-10-06 11:16:28 +0500

    user complete 4

--- api/auth/me.js
+++ api/auth/me.js
@@ -1,5 +1,5 @@
 import { AuthController } from "../../server/controllers/auth.controller.js"
-// import { verifyAuth } from "../../server/middleware/auth.js"
+import { verifyAuth } from "../../server/middleware/auth.js"
 
 
 export default async function handler(req, res) {
@@ -7,8 +7,8 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: "Method Not Allowed" })
   }
 
-//   const userId = verifyAuth(req, res)
-//   if (!userId) return 
+  const userId = verifyAuth(req, res)
+  if (!userId) return 
 
-  await AuthController.me(req, res)
+  await AuthController.me(req, res, userId)
 }
