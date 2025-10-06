5c32e03463ba20b6e9c9d102e299f2c66a978dda
alikamran0706 <alikamrantechdev@gmail.com>
2025-10-06 10:08:37 +0500

    test db 2

--- server/middleware/error-handler.js
+++
@@ -1,4 +0,0 @@
-export function errorHandler(err, _req, res, _next) {
-  const status = err.status || 400
-  res.status(status).json({ message: err.message || "Unexpected error" })
-}
--- server/routes/communications.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { auth } from "../middleware/auth.js"
-import { CommunicationsController } from "../controllers/communications.controller.js"
-
-export const communicationsRoutes = Router()
-communicationsRoutes.use(auth)
-communicationsRoutes.get("/", CommunicationsController.list)
-communicationsRoutes.patch("/:id/read", CommunicationsController.markRead)
